4b6f41336f2a1f01565fd29d692d1dedbe584443
Maria Fernanda Senosain <mariafernanda.senosain@vanderbilt.edu>
2020-12-08 12:30:11 -0600

    plots

--- R/20_Metabolon_vizfunctions.R
+++ R/20_Metabolon_vizfunctions.R
@@ -1,48 +1,82 @@
-# Select top n metabolites
-n <- 50
-top_met <- data_DH[1:n,]
+set_environment <- function(){
+    library(dplyr)
+    library(tidyverse)
+    library(tidyr)
+    library(ggplot2)
+    library(ComplexHeatmap)
+}
 
-ha = HeatmapAnnotation(
+dh_heatmap <- function(dt, col_names){
+
+    # HM annotations
+    col_ha = HeatmapAnnotation(
     condition = c('OE', 'WT'),
     simple_anno_size = unit(0.5, "cm"),
     show_legend = FALSE,
     show_annotation_name = FALSE
-)
+    )
 
-row_ha = HeatmapAnnotation(
-    Subtype=top_met$subtype,
-    simple_anno_size = unit(0.5, "cm"),
-    which = 'row',
-    show_annotation_name = FALSE
-)
+    row_ha = HeatmapAnnotation(
+        Subtype=dt$subtype,
+        simple_anno_size = unit(0.5, "cm"),
+        which = 'row',
+        show_annotation_name = FALSE
+    )
 
-Heatmap(scale(top_met[,3:4]), name = "z-score",
+    # Get index for cols of interest
+    idx <- which(colnames(dt) %in% col_names)
+
+    # Plot heatmap
+    print(Heatmap(scale(dt[,idx]), 
+        name = "z-score",
         heatmap_legend_param = list(color_bar = "continuous"), 
         show_row_dend = T,
         show_column_dend = FALSE,
         show_column_names = FALSE,
         show_row_names = FALSE,
-        row_split = top_met$subtype,
-        column_split = c('OE', 'WT'),
+        row_split = dt$subtype,
+        column_split = col_names,
         right_annotation = row_ha,
-        top_annotation = ha, 
-        row_title=NULL)
+        top_annotation = col_ha, 
+        row_title=NULL))
+}
 
 
-max_metabolites <- 50
-curated_metabolites <- data_DH %>%
+
+L2FC_barplot <- function(dt, max_metabolites = 20, msubtype) {
+
+    # Color function
+    color_levels <- function(dt) {
+        colors <- c()
+        if (any(dt$state == "DN")) {
+          colors <- c(colors, "lightblue")
+        }
+        if (any(dt$state == "UP")) {
+          colors <- c(colors, "#DC143C")
+        }
+        colors
+    }
+
+    # Select top metabolites by subtype
+    dt <- dt %>% 
+            filter(subtype==msubtype) %>%
+            arrange(desc(abs(L2R))) %>%
             dplyr::slice(1:max_metabolites)
-#curated_pathways['leadingEdge'] <- NULL
-ggplot(curated_metabolites, aes(reorder(metabolite, L2R), L2R)) +
-    geom_col(aes(fill = state), width = 0.5, color = "black") +
-    scale_size_manual(values = c(0, 1), guide = "none") +
-    #geom_label(aes(label = round(padj, 4)), size = 3) +
-    coord_flip() +
-    labs(
-        x = 'Metabolite', 
-        y = "Log2 fold-change"
-        #title = str_c(pathways_title, " pathways: ", condition_name),
-        #subtitle = str_c("(Cutoff: p.adj <", cutoff, ")")
-    ) +
-    theme_bw() 
-    #scale_fill_manual(values = color_levels(curated_metabolites))
+
+    print(ggplot(dt, aes(reorder(metabolite, L2R), L2R)) +
+            geom_col(aes(fill = state), width = 0.5, color = "black") +
+            scale_size_manual(values = c(0, 1), guide = "none") +
+            coord_flip() +
+            labs(
+                x = 'Metabolite', 
+                y = "Log2 fold-change",
+                title = paste0("Top ", max_metabolites, 
+                    " deregulated metabolites: ", msubtype)
+            ) +
+            theme_bw() +
+            scale_fill_manual(values = color_levels(dt))
+    )
+
+}
+
+
